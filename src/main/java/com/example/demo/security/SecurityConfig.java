96972bb2cdda2799989a3e9e0d27ca7251b7e1e9
attila-h <hauth.attila@gmail.com>
2020-10-27 17:50:21 +0100

    add SecurityConfig configuration for spring security

---
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/auth/signin").permitAll() // allowed by anyone
+                .antMatchers(HttpMethod.GET, "/vehicles/**").authenticated() // allowed only when signed in
+                .antMatchers(HttpMethod.DELETE, "/vehicles/**").hasRole("ADMIN") // allowed if signed in with ADMIN role
+                .anyRequest().denyAll(); // anything else is denied
+    }
+}
