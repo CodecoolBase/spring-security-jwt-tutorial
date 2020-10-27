777eefb52e3d3599b3c2d346a20c9cb36c5829a9
attila-h <hauth.attila@gmail.com>
2020-10-27 17:42:44 +0100

    add password encoder to DataInitializer

--- src/main/java/com/example/demo/DataInitializer.java
+++ src/main/java/com/example/demo/DataInitializer.java
@@ -7,6 +7,8 @@ import com.example.demo.repository.VehicleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -19,9 +21,12 @@ public class DataInitializer implements CommandLineRunner {
 
     private final UserRepository users;
 
+    private final PasswordEncoder passwordEncoder;
+
     public DataInitializer(VehicleRepository vehicles, UserRepository users) {
         this.vehicles = vehicles;
         this.users = users;
+        passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     @Override
@@ -34,17 +39,18 @@ public class DataInitializer implements CommandLineRunner {
 
         users.save(VehicleAppUser.builder()
             .username("user")
-            .password("password")
+            .password(passwordEncoder.encode("password"))
             .roles(Arrays.asList("ROLE_USER"))
             .build()
         );
 
         users.save(VehicleAppUser.builder()
             .username("admin")
-            .password("password")
+            .password(passwordEncoder.encode("password"))
             .roles(Arrays.asList("ROLE_USER", "ROLE_ADMIN"))
             .build()
         );
+
         log.debug("printing all users...");
         users.findAll().forEach(v -> log.debug(" VehicleAppUser :" + v.toString()));
     }
