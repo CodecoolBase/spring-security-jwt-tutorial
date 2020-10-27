711b9f05c9551709eb85fcc99fd5b615f41f879f
attila-h <hauth.attila@gmail.com>
2020-10-27 18:09:34 +0100

    add UserCredentials model

---
+++ src/main/java/com/example/demo/model/UserCredentials.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCredentials {
+    private String username;
+    private String password;
+}
